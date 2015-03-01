33cba6df20a44ac7e340cbdf1e0c6380cdf9ead3
Piotr <stecpiotr1991@gmail.com>
2015-03-01 21:18:54 +0100

    Task 4 Add validations and average method in Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :price, :title, :description, presence: true
+  validates :price, format: { with: /\A\d+??(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
+
+  def average_rating
+    reviews.average(:rating)
+  end
 end
